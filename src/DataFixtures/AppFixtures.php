9a5bfd9c98fb2c8ef034e912f71c5737f41ac0d2
= <=>
2021-03-27 17:26:24 +0100

    Reset branch

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,47 +16,5 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-  /*      for($i = 0; $i < 5; $i++){
-            $categorie = new Categorie();
-            $categorie->setNom("categ".$i);
-            $manager->persist($categorie);
-        }
-
-        $personne = new Personne();
-        $personne->setNom("Paure");
-        $personne->setPrenom("Alain");
-        $personne->setType("Dessinateur");
-        $manager->persist($personne);
-
-        $personne = new Personne();
-        $personne->setNom("Doe");
-        $personne->setPrenom("John");
-        $personne->setType("Scenariste");
-        $manager->persist($personne);
-
-        for($l = 0; $l < 5; $l++){
-            $editeur = new Editeur();
-            $editeur->setNom("Editeur".$l);
-            $manager->persist($editeur);
-
-        }
-
-        for($m = 0; $m < 5; $m++){
-            $serie = new Serie();
-            $serie->setNom("Serie".$m);
-            $serie->setCategorie($m);
-            $serie->setScenariste(1);
-            $serie->setDessinateur(2);
-            $serie->setEditeur($m);
-            $serie->setEtat("En cours de parution");
-            $manager->persist($serie);
-        }
-
-        for($n = 0; $n < 5; $n++){
-            $manga = new Manga();
-            $manga->setSerie();
-        }*/
-
-
     }
 }
